4c67c9fd241f9cc10aea476b1886611de2c5bb47
mlshawqi <marriyash77@.com>
2025-09-05 18:46:04 +0100

    analog clock76

--- main.c
+++ main.c
@@ -29,40 +29,41 @@ typedef struct s_point3d
 } t_point3d;
 
 
-int main(void)
+//analog clock
+int     main(void)
 {
-    void *mlx = mlx_init();
-    void *win = mlx_new_window(mlx, 400, 300, "Clock Example");
+        void *mlx = mlx_init();
+        void *win = mlx_new_window(mlx, 400, 300, "Clock");
 
-    int canvas_w = 400;
-    int canvas_h = 300;
-    double scale = 100.0; // scale factor for visibility
+        int canvas_w = 400;
+        int canvas_h = 300;
+        double scale = 100.0;
 
-    // 12 o'clock in 3D (x,z plane)
-    Tuple p = {0, 0, 1, 1};
 
-    // Draw all 4 main hours: 12, 3, 6, 9
-    for (int i = 0; i < 12; i++)
-    {
-        Tuple temp = p; // copy point
-        Matrix *r = rotation_y(i * M_PI / 6);
+        Tuple p = {0, 0, 1, 1};
 
-        // Multiply point by rotation matrix
-        temp = multiply_matrix_tuple(*r, temp);
 
-        // Map 3D point to 2D screen (x-z plane)
-        int screen_x = (int)(temp.x * scale + canvas_w / 2);
-        int screen_y = (int)(temp.z * scale + canvas_h / 2);
+        for (int i = 0; i < 12; i++)
+        {
+                Tuple temp = p;
+                Matrix *r = rotation_y(i * M_PI / 6);
 
-        mlx_pixel_put(mlx, win, screen_x, screen_y, 0x00FF00);
 
-        free_matrix(r); 
-    }
+                temp = multiply_matrix_tuple(*r, temp);
 
-    mlx_loop(mlx);
 
-    mlx_destroy_window(mlx, win);
-    mlx_destroy_display(mlx);
-    free(mlx);
-    return 0;
+                int screen_x = (int)(temp.x * scale + canvas_w / 2);
+                int screen_y = (int)(temp.z * scale + canvas_h / 2);
+
+                mlx_pixel_put(mlx, win, screen_x, screen_y, 0x00FF00);
+
+                free_matrix(r); 
+        }
+
+        mlx_loop(mlx);
+
+        mlx_destroy_window(mlx, win);
+        mlx_destroy_display(mlx);
+        free(mlx);
+        return 0;
 }
